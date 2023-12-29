64204c9b7244bf5146419e4ac8a2bb10ec80d7bd
Talha50220 <155101033+Talha50220@users.noreply.github.com>
2023-12-29 16:35:24 +0500

    SignInScreen.ios.js

---
+++ SignInScreen/SignInScreen.ios.js
@@ -0,0 +1,10 @@
+// SignInScreen.ios.js
+
+import React from 'react';
+import SignIncreen from '../../screens/SignInScreen';
+
+const SignInScreenIOS = (props) => {
+ 
+};
+
+export default SignInScreenIOS;
