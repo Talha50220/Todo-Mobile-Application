13b1c95177056b48a9a18323af3532e7c16fd387
Talha50220 <155101033+Talha50220@users.noreply.github.com>
2023-12-29 16:34:32 +0500

    SignUpScreen.android.js

---
+++ SignUpScreen/SignUpScreen.android.js
@@ -0,0 +1,10 @@
+// SignUpScreen.android.js
+
+import React from 'react';
+import SignnUpcreen from '../../screens/SignUpScreen';
+
+const SignUpcreenAndroid = (props) => {
+ 
+};
+
+export default SignUpScreenAndroid;
