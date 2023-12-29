748786e0e50b00b56ac713dd64dad08f7889ca9a
Talha50220 <155101033+Talha50220@users.noreply.github.com>
2023-12-29 16:33:44 +0500

    SignInScreen.android.js

---
+++ SignUpScreen/SignInScreen.android.js
@@ -0,0 +1,10 @@
+// SignInScreen.android.js
+
+import React from 'react';
+import SignnIncreen from '../../screens/SignInScreen';
+
+const SignInScreenAndroid = (props) => {
+ 
+};
+
+export default SignInScreenAndroid;
