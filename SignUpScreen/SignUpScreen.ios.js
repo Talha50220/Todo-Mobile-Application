14db7e2997a3530ba48385a0b837405c3a8add16
Talha50220 <155101033+Talha50220@users.noreply.github.com>
2023-12-29 16:32:46 +0500

    SignUpScreen.ios.js

---
+++ SignUpScreen/SignUpScreen.ios.js
@@ -0,0 +1,10 @@
+// SignUpScreen.ios.js
+
+import React from 'react';
+import SignUpScreen from '../../screens/SignUpScreen';
+
+const SignUpcreenIOS = (props) => {
+ 
+};
+
+export default SignUpScreenIOS;
